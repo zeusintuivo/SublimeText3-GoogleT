52445c000c3accc6cfcede28d540a2c619fbed87
zeusintuivo <zeus@intuivo.com>
2019-07-10 21:08:40 +0200

    added more testing features

--- core/translate.py
+++ core/translate.py
@@ -103,36 +103,76 @@ class GoogletTranslate(object):
 
     def translate(self, text, target_language, source_language, formato='html'):
         original = unquote(quote(text, ''))
+        print('original:', original)
+        if "'" in original:
+            original = original.replace("'", '"')
+        print('original quo:', original)
         if formato == 'plain':
-            data = self._get_translation_from_google(text)
+            data = self._get_translation_from_google(original)
             data = self.filter_tags(data)
         elif formato == 'yml':
-            if len(original) > 250:
+            if len(original) > 256:
                 print('1')
-                data = self.fix_too_text(original)
+                data = self.fix_too_long_text(original)
             else:
                 print('2')
-                if '%{' in original:
-                    print('3')
-                    data = self.fix_variable_keep(original)
+                if self.is_it_just_a_key(original):
+                    if original == source_language + ':':  # change fr: to es:
+                        data = target_language + ':'
+                    else:
+                        data = original
                 else:
-                    data = self._get_translation_from_google(original)
-                data = self.fix_yml(original, data, target_language, source_language)
+                    if '<' in original:
+                        print('3')
+                        data = self.fix_html_keep(original)
+                    elif '%{' in original:
+                        print('4')
+                        data = self.fix_variable_keep(original)
+                    else:
+                        data = self._get_translation_from_google(original)
+                    data = self.fix_yml(original, data, target_language, source_language)
         else:
             data = self._get_translation_from_google(text)
             data = self.fix_google(data)
         return data
 
-    def fix_too_text(self, original):
-        if len(original) > 250:
-            long_data = ""
+    @staticmethod
+    def is_it_just_a_key(original):
+        print(10)
+        original_no_spaces = original.lstrip()
+        original_no_spaces_all = original_no_spaces.rstrip()
+        if original_no_spaces_all in (None, '', '<br/>', '</i>', '<strong>', '</strong>',
+                                      '<i>', '<br>', '</br>'):  # skip empty br's
+            return True
+        print(11)
+        original_key_is = original_no_spaces.split(':')
+        print(12)
+        key_has_spaces = original_key_is[0].split(' ')
+        print(13)
+        second_part_exists = ""
+        if len(original_key_is) > 1:
+            second_part_exists = original_key_is[1].lstrip().rstrip()
+        if ':' in original and ':' in original and len(original_key_is) >= 2 and len(key_has_spaces) == 1:
+            print('row has a yml key:(' + original + ')')
+            if second_part_exists in (None, '', '>', '|'):
+                # empty second meaning, then is a like == key: or key:>  or key: |
+                return True
+        return False
+
+    def fix_too_long_text(self, original):
+        sentence_data = original
+        if len(original) > 256:
             sentence_data = ""
             split_sentences = original.split('.')
             for sentence in split_sentences:
-                if '%{' in sentence:
+                if '<' in original:
+                    print('23')
+                    sentence_data = sentence_data + self.fix_html_keep(sentence)
+                elif '%{' in original:
+                    print('24')
                     sentence_data = sentence_data + self.fix_variable_keep(sentence)
                 else:
-                    sentence_data = self._get_translation_from_google(sentence)
+                    sentence_data = sentence_data + self._get_translation_from_google(sentence)
         return sentence_data
 
     def fix_variable_keep(self, sentence):
@@ -178,7 +218,52 @@ class GoogletTranslate(object):
             sentence_data = splitted_trans
         return sentence_data
 
-    def _get_translation_from_google(self, text):
+    def fix_html_keep(self, sentence):
+        sentence_data = ""
+        split_percent = sentence.split('<')
+        splitted_trans = ""
+        count_split = 0
+        for splitted in split_percent:
+            if splitted in (None, ''):
+                # case 1 "%{time_ago} Dernière connexion sur le compte : il y a %{%{time_ago}%{time_ago}.".split('%{')
+                # ['', 'time_ago} Dernière connexion sur le compte : il y a ', '', 'time_ago}', 'time_ago}.']
+                # splitted = split_percent[0]  -- '' = splitted_trans = '%{'
+                # splitted = split_percent[1]  -- 'time_ago} Dernière connexion sur le compte : il y a '
+                # splitted = split_percent[2]  -- ''
+                # splitted = split_percent[3]  -- 'time_ago}'
+                # splitted = split_percent[4]  -- 'time_ago}'
+                # -
+                # case 2 "%{details_link}"
+                # ['', 'details_link}']
+                splitted_trans = splitted_trans + ' <'
+            else:
+                if '>' in splitted:
+                    # 'time_ago} Dernière connexion sur le compte : il y a '
+                    cut_other_part = splitted.split('>')
+                    # ['time_ago', ' Dernière connexion sur le compte : il y a ']
+                    second_part_split = cut_other_part[1]
+                    #              ' Dernière connexion sur le compte : il y a '
+                    if second_part_split in (None, ''):
+                        splited_data = ''
+                    else:
+                        splited_data = self.fix_variable_keep(second_part_split)
+                        # splited_data = self._get_translation_from_google(second_part_split)
+                    if count_split == 0:
+                        splitted_trans = splitted_trans + cut_other_part[0] + '> ' + splited_data
+                    else:
+                        splitted_trans = splitted_trans + ' <' + cut_other_part[0] + '> ' + splited_data
+                else:
+                    splited_data = self.fix_variable_keep(splitted)
+                    # splited_data = self._get_translation_from_google(splitted)
+                    splitted_trans = splitted_trans + splited_data
+                count_split = count_split + 1
+        if count_split == 0:
+            sentence_data = sentence_data + ' <' + splitted_trans
+        else:
+            sentence_data = splitted_trans
+        return sentence_data
+
+    def _get_translation_from_deepl(self, text):
         try:
             json5 = self._get_json5_from_google(text).decode('utf-8')
         except IOError:
@@ -200,7 +285,7 @@ class GoogletTranslate(object):
                 else:
                     opener = build_opener(SocksiPyHandler(PROXY_TYPE_HTTP, self.proxyho, int(self.proxypo)))
             request_url = self.api_urls['translate'] + "&sl=%s&tl=%s&text=%s" % (
-            self.source, self.target, escaped_source)
+                self.source, self.target, escaped_source)
             print('request_url:' + request_url)
             req = Request(request_url, headers=headers)
             result = opener.open(req, timeout=2).read()
@@ -302,7 +387,7 @@ class GoogletTranslate(object):
                 sz = s.search(html_string)
         # this is a key     in yml --> last_connection_html:
         # this is not a key in yml --> Dernière connexion sur le compte :
-        if ':' in original and ':' in html_string and len(original_key_is) >= 2 and len(key_has_spaces) == 1:
+        if ':' in original and ':' in html_string and len(original_key_is) >= 2 and len(key_has_spaces) == 1:  # fix keep keys names
             print('yml key protection:' + original + ')')
             first_source_colon = original.find(':')
             keep_source_definition = original[:first_source_colon]
@@ -315,12 +400,11 @@ class GoogletTranslate(object):
         print('original(' + original + ')')
         # print('source_language(' + source_language + ')')
         # print('target_language(' + target_language + ')')
-        if '{' in original and '{' in html_string and '%' in original and '%' in html_string:
+        if '{' in original and '{' in html_string and '%' in original and '%' in html_string:   # fix  % { to  %{
             html_string = html_string.replace('% {', ' %{')
-        if ': >' in original and ':>' in html_string:
+        if ': >' in original and ':>' in html_string:      # fix :> to : >
             html_string = html_string.replace(':>', ': >')
-        if original == source_language + ':':
-            html_string = target_language + ':'
+
         # restore white spaces
         html_string_no_spaces = html_string.lstrip()
         html_string_len = len(html_string)
@@ -350,8 +434,7 @@ class GoogletTranslate(object):
 
         return html_string
 
+    if __name__ == "__main__":
+        import doctest
 
-if __name__ == "__main__":
-    import doctest
-
-    doctest.testmod()
+        doctest.testmod()
